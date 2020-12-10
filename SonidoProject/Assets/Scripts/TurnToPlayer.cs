0c3ad3877b2b4ed4a5b47d3f96d7d79aea3ee9f1
filinouns <andpuent@ucm.es>
2020-12-10 02:04:44 +0100

    Zombie

---
+++ SonidoProject/Assets/Scripts/TurnToPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnToPlayer : MonoBehaviour {
+
+    public GameObject _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = _player.GetComponent<Transform>().position;
+        playerPos.y -= 3;
+        transform.LookAt(playerPos);
+        //transform.rotation.Set(playerPos.x, playerPos.y, playerPos.z, 0);
+        //print("Update rotation" + transform.rotation);
+        //print("PlayerPos" + playerPos);
+    }
+}
