ba3f29d5c22bdaffbc211a9f43ee97db3883c58a
filinouns <andpuent@ucm.es>
2021-01-16 12:02:36 +0100

    Avanzando en escena

--- SonidoProject/Assets/Scripts/TurnToPlayer.cs
+++ SonidoProject/Assets/Scripts/TurnToPlayer.cs
@@ -1,24 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TurnToPlayer : MonoBehaviour {
 
     public GameObject _player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         Vector3 playerPos = _player.GetComponent<Transform>().position;
         playerPos.y -= 3;
         transform.LookAt(playerPos);
-        //transform.rotation.Set(playerPos.x, playerPos.y, playerPos.z, 0);
-        //print("Update rotation" + transform.rotation);
-        //print("PlayerPos" + playerPos);
+
+        float distX = transform.position.x - _player.transform.position.x;
+        float distZ = transform.position.z - _player.transform.position.z;
+        if ((distX < 10 && distX > -10) && (distZ < 10 && distZ > -10)) {
+            GetComponent<Animator>().SetBool("StopMovement", true);
+        } else
+        {
+            GetComponent<Animator>().SetBool("StopMovement", false);
+        }
     }
 }
